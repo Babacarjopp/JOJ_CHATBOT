76baa431a12d06fb56a1b4196f89f27f0b6786e9
Babacarjoop <elouboy269@gmail.com>
2026-03-06 16:54:27 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+// backend/server.js
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.json({ status: "🦁 JOJ Backend OK" })
+})
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001")
+})
